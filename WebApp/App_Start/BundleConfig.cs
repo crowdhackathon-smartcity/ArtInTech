41d9eb1e73d8015e174370e8fbf14ad4aebc91d1
Vasileios Angelopoulos <bill_aggelopoylos@hotmail.com>
2017-05-12 21:03:32 +0300

    Ensomatosi Plugin part2

--- WebApp/App_Start/BundleConfig.cs
+++ WebApp/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace WebApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +26,13 @@ namespace WebApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
+                      "~/Scripts/admin.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/admin").Include(
+                      "~/Content/AdminLTE.min.css",
+                      "~/Content/skins/_all-skins.min.css"));
         }
     }
 }
