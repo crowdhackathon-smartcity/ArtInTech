175661761d1338fdd5986bdb854bd27e1d549796
Vasileios Angelopoulos <bill_aggelopoylos@hotmail.com>
2017-05-12 22:56:54 +0300

    Prosthiki tou controles gia Layouts

---
+++ WebApp/Models/ViewModel/ViewLayout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models.ViewModel
+{
+    public class ViewLayout
+    {
+        public DevLayout Devlayout { get; set; }
+        public string result { get; set; }
+    }
+}
